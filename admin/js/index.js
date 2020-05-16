6139434c30014bc2a3d97945a8a5f23d7a7abec2
chen2rong <2637342812@qq.com>
2020-05-16 17:04:19 +0800

    主页获取用户信息完成

---
+++ admin/js/index.js
@@ -0,0 +1,22 @@
+$(function () {
+    // 1、发送Ajax请求
+    $.ajax({
+        type: 'get',
+        url: 'http://localhost:8080/api/v1/admin/user/info',
+        // 设置请求头，将本地存储的token发送给服务器，用于验证用户是否已经登录
+        headers: {
+            'Authorization': window.localStorage.getItem('token')
+        },
+        success: function (obj) {
+            // 2、请求回来的数据要渲染到页面
+            if (obj.code == 200) {
+                // 显示登录账号的用户名
+                $('.user_info span').html(`欢迎&nbsp;&nbsp;${obj.data.nickname}`)
+                // 显示登录账号的头像
+                $('.user_info img').attr('src', obj.data.userPic)
+                // 个人中心的头像也需要设置
+                $('.header_bar .user_center_link img').attr('src', obj.data.userPic)
+            }
+        }
+    })
+})
--- admin/js/login.js
+++ admin/js/login.js
@@ -1,36 +1,44 @@
 $(function () {
-    // 1、给表单注册提交事件
-    $('.login_form').on('submit', function (e) {
-        // 2、阻止默认提交行为
-        e.preventDefault()
-        // 发送Ajax请求
-        $.ajax({
-            type: 'post',
-            url: 'http://localhost:8080/api/v1/admin/user/login',
-            data: $(this).serialize(),
-            // 3、发送请求之前先验证用户名或密码是否为空
-            beforeSend: function () {
-                var flag = false
-                $('input[name]').each(function (index, item) {
-                    if ($.trim($(item).val()) == '') {
-                        flag = true
+    ; (function () {
+        // 1、给表单注册提交事件
+        $('.login_form').on('submit', function (e) {
+            // 2、阻止默认提交行为
+            e.preventDefault()
+            // 发送Ajax请求
+            $.ajax({
+                type: 'post',
+                url: 'http://localhost:8080/api/v1/admin/user/login',
+                data: $(this).serialize(),
+                // 3、发送请求之前先验证用户名或密码是否为空
+                beforeSend: function () {
+                    var flag = false
+                    $('input[name]').each(function (index, item) {
+                        if ($.trim($(item).val()) == '') {
+                            flag = true
+                        }
+                    })
+                    // 为空时阻止发送请求
+                    if (flag) {
+                        $('.modal').modal('show')
+                        $('.modal-body p').text('用户名和密码不能为空')
+                        return false
                     }
-                })
-                // 为空时阻止发送请求
-                if (flag) {
-                    alert('用户名和密码不能为空')
-                    return false
+                },
+                success: function (res) {
+                    $('.modal').modal('show')
+                    $('.modal-body p').text(res.msg)
+                    if (res.code == 200) {
+                        $('.modal').on('hidden.bs.modal', function (e) {
+                            // 用户名密码正确，将token存入localStorage
+                            window.localStorage.setItem('token', res.token)
+                            window.location.href = './index.html'
+                        })
+                    }
+                    // else {
+                    //     alert(res.msg)
+                    // }
                 }
-            },
-            success: function (res) {
-                if (res.code == 200) {
-                    alert('登录成功')
-                    window.location.href = './index.html'
-                }
-                else {
-                    alert(res.msg)
-                }
-            }
+            })
         })
-    })
+    })()
 })
